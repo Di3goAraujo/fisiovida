c57bbfed82904a6849d4a23ed316051b5cac76c8
Di3goAraujo <156376029+Di3goAraujo@users.noreply.github.com>
2026-03-21 15:45:05 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
-const CACHE = 'fisiovida-v1';
-const FILES = ['/index.html', '/manifest.json'];
+const CACHE = 'fisiovida-v2';
+const FILES = [
+  '/fisiovida/index.html',
+  '/fisiovida/manifest.json'
+];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
